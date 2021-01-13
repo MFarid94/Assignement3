8d5dd8c57a3561f89ba8a60af32ea08b514eab31
Muhammad Farid <mfhosni94@gmail.com>
2021-01-13 10:59:13 +0200

    Eighth problem
    Write a program that computes the nth term of the geometric series: 1, 3, 9, 27, … Run the program to compute the 10th term of the given series

--- Exercise8/Exercise8.c
+++ Exercise8/Exercise8.c
@@ -1,25 +1,26 @@
 /*
- * Exercise7.c
+ * Exercise8.c
  *
  *  Created on: Jan 13, 2021
  *      Author: Muhammad Farid
  */
 
 #include<stdio.h>
-#define SIZE 100
+#define SIZE 10
 
 int main(void){
 	int arr[SIZE];
-	arr[0]=1;
 	int i;
+	arr[0]=1;
 	int num_index;
 
 	scanf("%d",&num_index);
 
 	for(i=1 ; i<SIZE ; i++){
-		arr[i] = arr[i-1] + 2;
+		arr[i] = arr[i-1]*3;
 	}
 
-	printf("Element %d is %d" ,num_index, arr[num_index-1]);
+	printf("The element of index %d is %d",num_index,arr[num_index-1]);
+
 	return 0;
 }
